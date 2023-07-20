4e3e058cd7a4eed05c0ca687d4b40a8f3be06ca3
Fran Bosquet <frbosquet@gmail.com>
2023-07-20 17:04:57 +0200

    add postgres

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
+import { sql } from '@vercel/postgres'
 
-export default function Home() {
+export default async function Home() {
+  const { rows } = await sql`SELECT 1 + 3`
+
   return (
     <>
       <h1 className="text-4xl uppercase tracking-wide">My Look Book</h1>
