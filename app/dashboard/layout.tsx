9035318b363172788a8f8a5e0811a901c539540f
Fran Bosquet <frbosquet@gmail.com>
2023-07-28 23:17:43 +0200

    add layout for dashboard

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,34 @@
+import { Logo } from "@components/Logo"
+import { SignOutButton } from "@components/SIgnOutButton"
+import { hasUser } from '@lib/session'
+import { authOptions } from "@services/auth"
+import { getServerSession } from "next-auth"
+import { redirect } from "next/navigation"
+
+type Props = {
+  children: React.ReactNode
+}
+
+export default async function Layout({ children }: Props) {
+  const data = await getServerSession(authOptions)
+
+  if (!hasUser(data)) {
+    redirect('/')
+  }
+
+  const { user } = data
+
+  return <main className="flex flex-col min-h-screen">
+    <header className="flex gap-4 bg-white p-2 justify-between items-center border-b border-gray-300">
+      <Logo className="text-4xl -tracking-widest px-2">et</Logo>
+      <SignOutButton />
+      <img src={user.image as string} alt={user.name as string} className="rounded-full w-12 h-12" />
+    </header>
+
+    {children}
+
+    <footer className="flex gap-4 bg-white p-2 justify-between items-center border-t border-gray-300">
+      footer
+    </footer>
+  </main>
+};
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,17 +1,14 @@
 import { LoanAdd } from "@components/LoanAdd"
-import { Logo } from "@components/Logo"
-import { SignOutButton } from "@components/SIgnOutButton"
 import { getUrl } from "@lib/api"
-import { hasUser } from '@lib/session'
+import { getUser } from "@lib/session"
 import { Loan } from "@prisma/client"
 import { authOptions } from "@services/auth"
 import { getServerSession } from "next-auth"
-import { redirect } from "next/navigation"
 
 const getUserLoans = async (userId: string) => {
   const url = getUrl(`loan?userId=${userId}`)
 
-  const response = await fetch(url, { cache: 'no-store' })
+  const response = await fetch(url, { cache: 'no-store', credentials: 'include' })
   const loans: Loan[] = await response.json()
 
   return loans
@@ -20,32 +17,16 @@ const getUserLoans = async (userId: string) => {
 export default async function Page() {
   const data = await getServerSession(authOptions)
 
-  if (!hasUser(data)) {
-    redirect('/')
-  }
-
-  const { user } = data
+  const user = getUser(data)
 
   const loans = await getUserLoans(user.id as string)
 
   return (
-    <main className="flex flex-col min-h-screen">
-      <header className="flex gap-4 bg-white p-2 justify-between items-center border-b border-gray-300">
-        <Logo className="text-4xl -tracking-widest px-2">et</Logo>
-        <SignOutButton />
-        <img src={user.image as string} alt={user.name as string} className="rounded-full w-12 h-12" />
-      </header>
-
-      <section className="flex-1 bg-white p-2">
-        <LoanAdd />
-        <h1 className="text-4xl uppercase">My<span className="text-gray-700">dashboard</span></h1>
-        <h3>Your loans</h3>
-        {loans.map(loan => <div key={loan.id}>{loan.name}</div>)}
-      </section>
-
-      <footer className="flex gap-4 bg-white p-2 justify-between items-center border-t border-gray-300">
-        footer
-      </footer>
-    </main>
+    <section className="flex-1 bg-white p-2">
+      <LoanAdd />
+      <h1 className="text-4xl uppercase">My<span className="text-gray-700">dashboard</span></h1>
+      <h3>Your loans</h3>
+      {loans.map(loan => <div key={loan.id}>{loan.name}</div>)}
+    </section>
   )
 }
