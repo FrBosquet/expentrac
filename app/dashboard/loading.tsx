35dc2d8bdf4409540391e4af19f586424eb19909
Fran Bosquet <frbosquet@gmail.com>
2023-08-05 10:30:23 +0200

    add loading screen

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,7 @@
+import { Spinner } from "@components/ui/spinner"
+
+export default function Loading() {
+  return <div className="flex-1 flex justify-center items-center">
+    <Spinner className="w-8 h-8" />
+  </div>
+}
