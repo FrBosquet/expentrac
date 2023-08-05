aca377e065ca2d40c10cc5e78e1399a0889ba470
Fran Bosquet <frbosquet@gmail.com>
2023-08-05 14:38:06 +0200

    delete signout button

--- components/SIgnOutButton.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import { signOut } from "next-auth/react"
-import { Button } from "./ui/button"
-
-export const SignOutButton = () => {
-  return <Button onClick={() => signOut()}>Sign Out</Button>
-}
