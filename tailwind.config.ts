4376fb272fe805a3b9428effe6cfef5b672dee03
Fran Bosquet <frbosquet@gmail.com>
2023-08-10 09:05:49 +0200

    clean up TW

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import { Config } from "tailwindcss"
+import { tailwindPreset } from "./lib/tailwind-preset"
+
+const config = {
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  presets: [tailwindPreset]
+} satisfies Config
+
+export default config
