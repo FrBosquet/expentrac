407ecff46c0a1d9db66a3621926a70fa8de75eee
Fran Bosquet <frbosquet@gmail.com>
2023-08-19 09:53:23 +0200

    add loans and subscription pages

---
+++ app/dashboard/subscriptions/page.tsx
@@ -0,0 +1,7 @@
+import { SubscriptionSummary } from "@components/subscription/summary"
+
+export default function page() {
+  return <section className="flex-1 bg-white w-screen max-w-3xl p-12 mx-auto">
+    <SubscriptionSummary />
+  </section>
+}
