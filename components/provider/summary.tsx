cb84c6e3e8f272b2f14e3273b3f7777fb0db04e4
Fran Bosquet <frbosquet@gmail.com>
2023-08-22 18:19:30 +0200

    add provider filter

--- components/provider/summary.tsx
+++ components/provider/summary.tsx
@@ -1,14 +1,37 @@
 'use client'
 
+import { Input } from '@components/ui/input'
+import { Plus, Search } from 'lucide-react'
+import { useMemo, useState, type ChangeEvent } from 'react'
+import { ProviderAdd } from './add'
 import { UserProviderCard } from './card'
 import { useProviders } from './context'
 
 export const UserProviderSummary = () => {
+  const [nameFilter, setNameFilter] = useState('')
+
   const { providers } = useProviders()
 
-  return <section className="grid grid-cols-3 gap-4">
-    {providers.map((userProvider) => {
-      return <UserProviderCard key={userProvider.id} userProvider={userProvider} />
-    })}
-  </section>
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setNameFilter(e.target.value)
+  }
+
+  const filteredProviders = useMemo(() => providers.filter(({ provider }) => {
+    return provider.name.toLowerCase().includes(nameFilter.toLowerCase())
+  }), [nameFilter, providers])
+
+  return <div className='flex flex-col gap-4'>
+    <section className='grid grid-cols-[auto_1fr_auto] gap-2 items-center'>
+      <Search size={16} className='text-slate-500' />
+      <Input value={nameFilter} onChange={handleChange} placeholder="Filter by name" />
+      <ProviderAdd className='h-full'>
+        <Plus size={14} />
+      </ProviderAdd>
+    </section>
+    <section className="grid grid-cols-3 gap-4">
+      {filteredProviders.map((userProvider) => {
+        return <UserProviderCard key={userProvider.id} userProvider={userProvider} />
+      })}
+    </section>
+  </div>
 }
