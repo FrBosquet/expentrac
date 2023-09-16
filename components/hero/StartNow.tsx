e6722df81101f8ed31910a9672b7db9a6b77a8dc
Fran Bosquet <frbosquet@gmail.com>
2023-09-16 19:40:34 +0200

    improve start now section

--- components/hero/StartNow.tsx
+++ components/hero/StartNow.tsx
@@ -3,11 +3,12 @@ import { RegularSignin } from './signin'
 
 export const StartNow = () => {
   return <section className='h-[80vh] bg-gradient-to-t from-slate-900 to-slate-800'>
-    <div className='h-full gap-6 max-w-screen-xl w-full items-center p-6 m-auto grid grid-cols-2 justify-center'>
+    <div className='h-full gap-12 max-w-screen-xl w-full items-center p-6 m-auto justify-center
+      flex
+    '>
+      <Rocket className='text-primary-600 drop-shadow-lg' size={270} />
 
-      <Rocket className='m-auto text-primary-600 drop-shadow-lg' size={270} />
-
-      <article className='flex flex-col gap-8 w-full justify-center items-start'>
+      <article className='flex flex-col gap-8 justify-center items-start'>
         <h1 className='text-6xl scroll-anim-fall text-primary-600'>Start tracking today</h1>
         <p className='text-slate-400'>Expentrac is free to use. You just need an email account.</p>
         <RegularSignin />
