edc1cdcba54b90b77f944af7f91eaa098049f115
Fran Bosquet <frbosquet@gmail.com>
2023-09-16 18:13:56 +0200

    clean up start now component

--- components/hero/StartNow.tsx
+++ components/hero/StartNow.tsx
@@ -4,9 +4,8 @@ import { RegularSignin } from './signin'
 export const StartNow = () => {
   return <section className='h-[80vh] bg-gradient-to-t from-slate-900 to-slate-800'>
     <div className='h-full gap-6 max-w-screen-xl w-full items-center p-6 m-auto grid grid-cols-2 justify-center'>
-      <article className='perspective-container self-center'>
-        <Rocket className='m-auto text-primary-600' size={270} />
-      </article>
+
+      <Rocket className='m-auto text-primary-600 drop-shadow-lg' size={270} />
 
       <article className='flex flex-col gap-8 w-full justify-center items-start'>
         <h1 className='text-6xl animate-scroll-from-above text-primary-600'>Start tracking today</h1>
