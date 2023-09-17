1fbb7135e3705ad51d83b45875f180aabf3aebd0
Fran Bosquet <frbosquet@gmail.com>
2023-09-17 19:57:23 +0200

    remove errors

--- components/hero/Wheel.tsx
+++ components/hero/Wheel.tsx
@@ -10,14 +10,14 @@ export const Wheel = () => {
     h-screen md:h-[110vh] md:-top-[5vh]
     left-[20vw] xl:left-[30vw]
     rounded-full border border-slate-200/40 justify-center items-center overflow-hidden perspective-wheel shadow-bloom-md bg-slate-600 hidden sm:flex scroll-anim-fade'>
-    <Image src={ImgCoin} placeholder='blur' layout='cover' alt='coins' className='absolute h-full w-full inset-0 bg-cover opacity-20 animate-spin-slowest saturate-0' />
+    <Image src={ImgCoin} placeholder='blur' alt='coins' className='absolute h-full w-full inset-0 bg-cover opacity-20 animate-spin-slowest saturate-0' />
 
     <div className='h-4/6 aspect-square rounded-full border border-slate-200/50 bg-slate-700 absolute shadow-bloom-md overflow-hidden -translate-x-5'>
-      <Image src={ImgVods} placeholder='blur' layout='cover' alt='coins' className='absolute h-full w-full inset-0 bg-cover saturate-0 opacity-25 animate-spin-slower' />
+      <Image src={ImgVods} placeholder='blur' alt='coins' className='absolute h-full w-full inset-0 bg-cover saturate-0 opacity-25 animate-spin-slower' />
     </div>
 
     <div className='h-2/6 aspect-square rounded-full border border-slate-400/50 absolute shadow-bloom-md overflow-hidden bg-slate-800 -translate-x-10'>
-      <Image src={ImgFinance} placeholder='blur' layout='cover' alt='coins' className='absolute h-full w-full inset-0 bg-cover saturate-0 opacity-25 animate-spin-slow' />
+      <Image src={ImgFinance} placeholder='blur' alt='coins' className='absolute h-full w-full inset-0 bg-cover saturate-0 opacity-25 animate-spin-slow' />
     </div>
   </div>
 }
