5bc7a75ac12b116ff5f3880a32e46c765cd4d41e
Fran Bosquet <frbosquet@gmail.com>
2023-09-17 20:25:44 +0200

    fix blog

--- app/(web)/blog/page.tsx
+++ app/(web)/blog/page.tsx
@@ -5,13 +5,13 @@ import Link from 'next/link'
 export default async function Blog() {
   const posts = await getPosts()
 
-  return <section className='flex flex-col flex-1 gap-12 justify-start items-center relative p-16 md:p-0 w-full max-w-5xl m-auto'>
+  return <section className='flex flex-col flex-1 gap-2 justify-start items-center relative py-16 md:py-0 w-full max-w-5xl m-auto'>
     <SectionTitle>Blog</SectionTitle>
     {
       posts.map((post) => (
-        <Link key={post.slug} href={`/blog/${post.slug}`} className='text-xl flex gap-4 text-slate-300 hover:text-primary-300 py-4 group transition'>
-          <p className='text-slate-400 group-hover:text-primary-800 transition'>{new Date(post.date).toLocaleDateString()}</p>
-          {post.title}
+        <Link key={post.slug} href={`/blog/${post.slug}`} className='w-full text-xl flex gap-4 text-slate-300 hover:text-primary-300 group transition'>
+          <p className='text-slate-400 text-right group-hover:text-primary-800 transition text-sm flex-1'>{new Date(post.date).toLocaleDateString()}</p>
+          <p className='flex-[2]'>{post.title}</p>
         </Link>
       ))
     }
