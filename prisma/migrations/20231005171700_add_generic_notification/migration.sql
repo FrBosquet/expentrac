a963b12ec3d92f57a4c83b49d586ed96c073ad4a
Fran Bosquet <frbosquet@gmail.com>
2023-10-05 19:18:02 +0200

    add model

---
+++ prisma/migrations/20231005171700_add_generic_notification/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "Notification" (
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "type" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "ack" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Notification_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Notification" ADD CONSTRAINT "Notification_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
