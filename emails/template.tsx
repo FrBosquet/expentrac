33ffa94d7428c78b586631cd6846c30e7253a607
Fran Bosquet <frbosquet@gmail.com>
2023-10-07 17:05:03 +0200

    fix logo in emails

--- emails/template.tsx
+++ emails/template.tsx
@@ -27,9 +27,9 @@ export default function TemplateEmail({
           <Container className="bg-slate-200 border border-solid border-slate-100 rounded-lg my-[40px] mx-auto p-[20px] w-[465px] font-sans">
             <Section className="mt-[32px]">
               <Img
-                src={'https://expentrac.app/logo.svg'}
-                width="40"
-                height="40"
+                src={'https://expentrac.app/logo.png'}
+                width="80"
+                height="80"
                 alt="Expentrac Logo"
                 className="mx-auto"
               />
