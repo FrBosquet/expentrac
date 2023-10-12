868707401e59dcb7788c5cc98c46a2d982bd81c4
Fran Bosquet <frbosquet@gmail.com>
2023-10-12 19:56:50 +0200

    improve hideable sidebar

--- components/NavigationMenu.tsx
+++ components/NavigationMenu.tsx
@@ -11,6 +11,7 @@ import { Separator } from './ui/separator'
 import { UserMenu } from './user/menu'
 
 const SEPARATOR = 'separator'
+const PUSHER = 'pusher'
 
 const components = [
   {
@@ -37,6 +38,7 @@ const components = [
     Icon: Landmark
   },
   SEPARATOR,
+  PUSHER,
   {
     title: 'Blog',
     href: '/blog',
@@ -51,31 +53,40 @@ const Sidebar = () => {
   const { user, loading } = useUser()
   const pathname = usePathname()
 
-  return <aside className='bg-card py-10 flex flex-col items-center gap-10 fixed h-screen border-r shadow-xl z-50
-      w-20 xl:w-56
+  return <aside className='group bg-card py-10 flex flex-col items-center gap-10 fixed h-screen border-r shadow-xl z-50
+      w-20 hover:w-56 transition-all overflow-hidden
+      xl:w-56
     '>
     <Logo className='text-3xl -tracking-widest px-6 self-start hidden xl:block'>expentrac</Logo>
-    <article className='flex flex-col gap-1 items-center w-full'>
+    <article className='flex flex-col gap-1 items-center w-full relative'>
       <UserMenu className='xl:w-20 xl:h-20' />
-      <h2 className='text-lg pt-4 hidden xl:block'>{loading ? '...' : user.name}</h2>
+      <h2 className='
+        text-sm pt-4 whitespace-nowrap opacity-0 transition h-0 absolute top-[2.5rem] uppercase font-light
+        group-hover:opacity-100
+        xl:text-md xl:relative xl:opacity-100 xl:top-0
+        '>{loading ? '...' : user.name}</h2>
     </article>
     <menu className='flex-1 flex flex-col w-full'>
       {
-        components.map((component) => {
+        components.map((component, index) => {
           if (typeof component === 'string') {
-            return <Separator key={component} />
+            const key = `component${index}`
+
+            if (component === SEPARATOR) {
+              return <Separator key={key} />
+            } else {
+              return <div key={key} className='flex-1' />
+            }
           }
 
           const { title, href, Icon } = component
           const isSelected = href === pathname
 
-          return <Link aria-disabled={isSelected} key={title} href={href} className={twMerge('group relative px-6 h-12 transition font-semibold text-sm uppercase hover:text-expentrac-800 hover:bg-theme-back dark:hover:text-expentrac flex items-center gap-2 hover:border xl:hover:border-0', isSelected && 'bg-theme-back text-expentrac-800 dark:text-expentrac-300 pointer-events-none')}>
+          return <Link aria-disabled={isSelected} key={title} href={href} className={twMerge('relative px-6 h-12 transition font-semibold text-sm uppercase hover:text-expentrac-800 hover:bg-theme-back dark:hover:text-expentrac flex items-center gap-2', isSelected && 'bg-theme-back text-expentrac-800 dark:text-expentrac-300 pointer-events-none')}>
             <Icon className='shrink-0 w-8 xl:w-4' />
             <p className='
-              opacity-20 bg-theme-back h-full flex items-center scale-x-0 origin-left transition pr-4
-              group-hover:opacity-100 group-hover:scale-x-100
-
-              xl:opacity-100 xl:scale-x-100 xl:bg-transparent xl:pr-0
+              h-full flex items-center origin-left pr-4 opacity-0
+              group-hover:opacity-100 xl:opacity-100
             '>{title}</p>
           </Link>
         })
