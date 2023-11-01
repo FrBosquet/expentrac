f2d7f79e0bf03392a68de5761f11a36bdea52bea
Fran Bosquet <frbosquet@gmail.com>
2023-11-01 18:28:09 +0100

    fix minor issues

--- app/dashboard/subscriptions/[id]/page.tsx
+++ app/dashboard/subscriptions/[id]/page.tsx
@@ -22,7 +22,7 @@ export default function Page() {
   return <>
     <h1 className='col-span-2 lg:col-span-4 text-5xl pb-4'>{sub.name}</h1>
     <menu className='flex gap-2 pb-6'>
-      <ButtonLink href='/dashboard/loans'><PinLeftIcon /> Back </ButtonLink>
+      <ButtonLink href='/dashboard/subscriptions'><PinLeftIcon /> Back </ButtonLink>
       {
         link
           ? <ButtonLink target='_blank' href={link}>
@@ -31,7 +31,7 @@ export default function Page() {
           : null}
       <SubEdit sub={sub} triggerDecorator={<article className="text-xs flex items-center gap-2"><Edit size={12} /> Edit</article>} />
       <SubDelete sideEffect={async () => {
-        push('/dashboard/loans')
+        push('/dashboard/subscriptions')
       }} triggerDecorator={<article className="text-xs flex items-center gap-2"><Trash size={12} /> Delete</article>} sub={sub} />
     </menu>
     <SubDetailContent sub={sub} className='col-span-2 lg:col-span-4' />
