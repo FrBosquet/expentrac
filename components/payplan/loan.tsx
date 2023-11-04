5c7e12cbd53c746551351a104c780182d70c8be6
FrBosquet <frbosquet@gmail.com>
2023-11-04 11:18:55 +0100

    feat: Add loan payplan tooltip

--- components/payplan/loan.tsx
+++ components/payplan/loan.tsx
@@ -1,3 +1,4 @@
+import { Tooltip } from '@components/Tooltip'
 import { useDate } from '@components/date/context'
 import { Card, CardContent, CardHeader, CardTitle } from '@components/ui/card'
 import { euroFormatter } from '@lib/currency'
@@ -12,9 +13,6 @@ export const LoanPayplan = ({ loan, className }: Props) => {
   const { date } = useDate()
   const { startDate, fee, payments, amount: { total } } = loan
 
-  // console.log({ startDate, endDate, fee, time, payments })
-
-  // number of months beetween startdate and enddate
   const payplan = new Array(payments.total).fill(null).map((_, index) => {
     if (index === 0) {
       return {
@@ -33,8 +31,6 @@ export const LoanPayplan = ({ loan, className }: Props) => {
     }
   })
 
-  console.log({ payplan })
-
   return <Card className={className}>
     <CardHeader>
       <CardTitle>Payment plan</CardTitle>
@@ -49,15 +45,21 @@ export const LoanPayplan = ({ loan, className }: Props) => {
             const isLast = index === payplan.length - 1
             const isThisMonth = paymentDate.getMonth() === date.getMonth() && paymentDate.getFullYear() === date.getFullYear()
 
-            return <article data-paid={isPaid} key={index} className='flex flex-row justify-between items-center data-[paid=true]:text-expentrac-800 hover:bg-theme-back p-1'>
-              <p className='text-xs font-semibold uppercase'>
-                {paymentDate.toLocaleDateString('default', { day: '2-digit', month: 'short', year: paymentDate.getFullYear() !== date.getFullYear() ? '2-digit' : undefined })}
-                {isFirst ? <span className='text-xs opacity-70'> (initial payment)</span> : null}
-                {isLast ? <span className='text-xs opacity-70'> (last payment)</span> : null}
-                {isThisMonth ? <span className='text-xs opacity-70'> (this month)</span> : null}
-              </p>
-              <p>{euroFormatter.format(amount)} <span className='text-xs opacity-70'>({euroFormatter.format(total - acc)} left)</span></p>
-            </article>
+            const tooltipContent = <aside>
+              <p>Paid: {euroFormatter.format(acc)}</p>
+            </aside>
+
+            return <Tooltip key={index} tooltip={tooltipContent}>
+              <article data-paid={isPaid} className='flex flex-row justify-between items-center data-[paid=true]:text-expentrac-800 hover:bg-theme-back p-1'>
+                <p className='text-xs font-semibold uppercase'>
+                  {paymentDate.toLocaleDateString('default', { day: '2-digit', month: 'short', year: paymentDate.getFullYear() !== date.getFullYear() ? '2-digit' : undefined })}
+                  {isFirst ? <span className='text-xs opacity-70'> (initial payment)</span> : null}
+                  {isLast ? <span className='text-xs opacity-70'> (last payment)</span> : null}
+                  {isThisMonth ? <span className='text-xs opacity-70'> (this month)</span> : null}
+                </p>
+                <p>{euroFormatter.format(amount)} <span className='text-xs opacity-70'>({euroFormatter.format(total - acc)} left)</span></p>
+              </article>
+            </Tooltip>
           })
         }
       </section>
