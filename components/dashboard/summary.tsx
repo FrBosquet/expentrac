23a7ff26c2af7a1e02dc54801fbed0c5b2edfa53
FrBosquet <frbosquet@gmail.com>
2023-11-13 21:22:29 +0100

    fix: build

--- components/dashboard/summary.tsx
+++ components/dashboard/summary.tsx
@@ -28,8 +28,10 @@ interface Props {
   className?: string
 }
 
-export const Summary = ({ className }: Props) => {
-  const { user, loading } = useUser()
+const Cards = ({ className, summary }: {
+  className: string
+  summary: ReturnType<typeof useSummary>
+}) => {
   const {
     owedMoney,
     totalFee,
@@ -37,32 +39,36 @@ export const Summary = ({ className }: Props) => {
     loanCount,
     subFee,
     subCount
-  } = useSummary()
+  } = summary
 
-  const renderCards = (className: string) => {
-    return <>
-      <ConceptCard className={className} hidden={owedMoney === 0} value={euroFormatter.format(owedMoney)} label="Total owed money" Icon={Coins} />
-      <ConceptCard className={className} hidden={totalFee === 0} value={euroFormatter.format(totalFee)} label="Total monthly payments" Icon={Receipt} />
-      <ConceptCard className={className} hidden={loanCount === 0} value={euroFormatter.format(loanFee)} label={`Total monthly loan payments, from ${loanCount} loans`} Icon={HelpingHand} />
-      <ConceptCard className={className} hidden={subCount === 0} value={euroFormatter.format(subFee)} label={`Total monthly subscription payments, from ${subCount} subscriptions`} Icon={Tv} />
-    </>
-  }
+  return <>
+    <ConceptCard className={className} hidden={owedMoney === 0} value={euroFormatter.format(owedMoney)} label="Total owed money" Icon={Coins} />
+    <ConceptCard className={className} hidden={totalFee === 0} value={euroFormatter.format(totalFee)} label="Total monthly payments" Icon={Receipt} />
+    <ConceptCard className={className} hidden={loanCount === 0} value={euroFormatter.format(loanFee)} label={`Total monthly loan payments, from ${loanCount} loans`} Icon={HelpingHand} />
+    <ConceptCard className={className} hidden={subCount === 0} value={euroFormatter.format(subFee)} label={`Total monthly subscription payments, from ${subCount} subscriptions`} Icon={Tv} />
+  </>
+}
 
-  return <><Card className={twMerge('bg-gradient-to-l from-expentrac-800 to-background relative', className)}>
-    <CardHeader className='relative z-20 py-8'>
-      <CardTitle className='text-5xl'>{loading ? '...' : `Hello ${user.name}`}</CardTitle>
-      <CardDescription>Whats going on with your money?</CardDescription>
-    </CardHeader>
-    <CardContent className='relative z-20'>
-      <div className='grid grid-cols-2 md:grid-cols-4 xl:grid-cols-5 gap-2'>
-        {renderCards('hidden md:block')}
+export const Summary = ({ className }: Props) => {
+  const { user, loading } = useUser()
+  const summary = useSummary()
+
+  return <>
+    <Card className={twMerge('bg-gradient-to-l from-expentrac-800 to-background relative', className)}>
+      <CardHeader className='relative z-20 py-8'>
+        <CardTitle className='text-5xl'>{loading ? '...' : `Hello ${user.name}`}</CardTitle>
+        <CardDescription>Whats going on with your money?</CardDescription>
+      </CardHeader>
+      <CardContent className='relative z-20'>
+        <div className='grid grid-cols-2 md:grid-cols-4 xl:grid-cols-5 gap-2'>
+          <Cards summary={summary} className='hidden md:block' />
+        </div>
+      </CardContent>
+      <Image src={Grid} alt="grid layout" className='absolute top-0 h-full w-full opacity-20 object-cover' />
+      <div className='absolute inset-0 overflow-hidden flex justify-end'>
+        <Image src={Money} alt="Expenses graphics" height={400} width={400} className='hidden md:block object-contain hue-rotate-180 w-[45%] xl:w-auto' />
       </div>
-    </CardContent>
-    <Image src={Grid} alt="grid layout" className='absolute top-0 h-full w-full opacity-20 object-cover' />
-    <div className='absolute inset-0 overflow-hidden flex justify-end'>
-      <Image src={Money} alt="Expenses graphics" height={400} width={400} className='hidden md:block object-contain hue-rotate-180 w-[45%] xl:w-auto' />
-    </div>
-  </Card>
-    {renderCards('md:hidden block')}
+    </Card>
+    <Cards summary={summary} className='block md:hidden' />
   </>
 }
--- components/notifications/context.tsx
+++ components/notifications/context.tsx
@@ -1,69 +1,7 @@
 'use client'
 
-import { type Notification } from '@lib/prisma'
-import { useResourceContext } from '@lib/resourceContext'
 import { useStore } from '@store'
 import { getPendingNotifications } from '@store/notification'
-
-import {
-  createContext,
-  type Dispatch,
-  type ReactNode,
-  type SetStateAction
-} from 'react'
-
-interface Props {
-  children: ReactNode
-  serverValue: Notification[]
-}
-
-const defaultContextValue = {
-  notifications: [],
-  setNotifications: () => null,
-  addNotification: () => null,
-  removeNotification: () => null,
-  updateNotification: () => null,
-  hasPending: false,
-  pending: 0
-}
-
-export const NotificationContext = createContext<{
-  notifications: Notification[]
-  setNotifications: Dispatch<SetStateAction<Notification[]>>
-  addNotification: (provider: Notification) => void
-  removeNotification: (provider: Notification) => void
-  updateNotification: (provider: Notification) => void
-  hasPending: boolean
-  pending: number
-}>(defaultContextValue)
-
-export const NotificationsProvider = ({ children, serverValue }: Props) => {
-  const {
-    resource: notifications,
-    setResource: setNotifications,
-    add: addNotification,
-    remove: removeNotification,
-    update: updateNotification
-  } = useResourceContext<Notification>(serverValue, (a, b) => b.createdAt > a.createdAt ? 1 : -1)
-
-  const pending = notifications.filter((notification) => !notification.ack).length
-
-  return (
-    <NotificationContext.Provider
-      value={{
-        notifications,
-        setNotifications,
-        addNotification,
-        removeNotification,
-        updateNotification,
-        hasPending: pending > 0,
-        pending
-      }}
-    >
-      {children}
-    </NotificationContext.Provider>
-  )
-}
 
 export const useNotifications = () => {
   const notifications = useStore(state => state.notifications)
